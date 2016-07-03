22ccd4646c95ee655ccc057030e0ba25cb22925c
Ben N <fleexy@mchsi.com>
2016-07-03 17:44:03 -0500

    Object-orient the rest of the ADMX structures

--- PolicyPlus/AdmxBundle.vb
+++ PolicyPlus/AdmxBundle.vb
@@ -1,11 +1,19 @@
 ﻿Imports System.IO
 Public Class AdmxBundle
+    Private SourceFiles As New Dictionary(Of AdmxFile, AdmlFile)
+    ' Temporary lists from ADMX files that haven't been integrated yet
     Private RawCategories As New List(Of AdmxCategory)
     Private RawProducts As New List(Of AdmxProduct)
     Private RawPolicies As New List(Of AdmxPolicy)
     Private RawSupport As New List(Of AdmxSupportDefinition)
-    Private SourceFiles As New Dictionary(Of AdmxFile, AdmlFile)
+    ' Lists that include all items, even those that are children of others
+    Private FlatCategories As New Dictionary(Of String, PolicyPlusCategory)
+    Private FlatProducts As New Dictionary(Of String, PolicyPlusProduct)
+    ' Lists of top-level items only
     Public Categories As New Dictionary(Of String, PolicyPlusCategory)
+    Public Products As New Dictionary(Of String, PolicyPlusProduct)
+    Public Policies As New Dictionary(Of String, PolicyPlusPolicy)
+    Public SupportDefinitions As New Dictionary(Of String, PolicyPlusSupport)
     Public Sub LoadFolder(Path As String, LanguageCode As String)
         For Each file In Directory.EnumerateFiles(Path)
             If file.ToLowerInvariant.EndsWith(".admx") Then AddSingleAdmx(file, LanguageCode)
@@ -27,17 +35,14 @@ Public Class AdmxBundle
         RawSupport.AddRange(admx.SupportedOnDefinitions)
         SourceFiles.Add(admx, adml)
     End Sub
-    Private Sub BuildStructures() ' TODO: the rest of the structures
+    Private Sub BuildStructures()
         Dim catIds As New Dictionary(Of String, PolicyPlusCategory)
-        Dim findCatById = Function(UniqueID As String) As PolicyPlusCategory
-                              If catIds.ContainsKey(UniqueID) Then
-                                  Return catIds(UniqueID)
-                              ElseIf Categories.ContainsKey(UniqueID) Then
-                                  Return Categories(UniqueID)
-                              Else
-                                  Return Nothing
-                              End If
-                          End Function
+        Dim productIds As New Dictionary(Of String, PolicyPlusProduct)
+        Dim supIds As New Dictionary(Of String, PolicyPlusSupport)
+        Dim polIds As New Dictionary(Of String, PolicyPlusPolicy)
+        Dim findCatById = Function(UID As String) FindInTempOrFlat(UID, catIds, FlatCategories)
+        Dim findSupById = Function(UID As String) FindInTempOrFlat(UID, supIds, SupportDefinitions)
+        Dim findProductById = Function(UID As String) FindInTempOrFlat(UID, productIds, FlatProducts)
         ' First pass: Build the structures without resolving references
         For Each rawCat In RawCategories
             Dim cat As New PolicyPlusCategory
@@ -46,23 +51,99 @@ Public Class AdmxBundle
             cat.RawCategory = rawCat
             catIds.Add(cat.UniqueID, cat)
         Next
+        For Each rawProduct In RawProducts
+            Dim product As New PolicyPlusProduct
+            product.DisplayName = ResolveString(rawProduct.DisplayCode, rawProduct.DefinedIn)
+            product.UniqueID = QualifyName(rawProduct.ID, rawProduct.DefinedIn)
+            product.RawProduct = rawProduct
+            productIds.Add(product.UniqueID, product)
+        Next
+        For Each rawSup In RawSupport
+            Dim sup As New PolicyPlusSupport
+            sup.DisplayName = ResolveString(rawSup.DisplayCode, rawSup.DefinedIn)
+            sup.UniqueID = QualifyName(rawSup.ID, rawSup.DefinedIn)
+            If rawSup.Entries IsNot Nothing Then
+                For Each rawSupEntry In rawSup.Entries
+                    Dim supEntry As New PolicyPlusSupportEntry
+                    supEntry.RawSupportEntry = rawSupEntry
+                    sup.Elements.Add(supEntry)
+                Next
+            End If
+            sup.RawSupport = rawSup
+            supIds.Add(sup.UniqueID, sup)
+        Next
+        For Each rawPol In RawPolicies
+            Dim pol As New PolicyPlusPolicy
+            pol.DisplayExplanation = ResolveString(rawPol.ExplainCode, rawPol.DefinedIn)
+            pol.DisplayName = ResolveString(rawPol.DisplayCode, rawPol.DefinedIn)
+            pol.UniqueID = QualifyName(rawPol.ID, rawPol.DefinedIn)
+            pol.RawPolicy = rawPol
+            polIds.Add(pol.UniqueID, pol)
+        Next
         ' Second pass: Resolve references and link structures
         For Each cat In catIds.Values
             If cat.RawCategory.ParentID <> "" Then
                 Dim parentCatName = ResolveRef(cat.RawCategory.ParentID, cat.RawCategory.DefinedIn)
                 Dim parentCat = findCatById(parentCatName)
-                If parentCat Is Nothing Then Continue For
+                If parentCat Is Nothing Then Continue For ' In case the parent category doesn't exist
                 parentCat.Children.Add(cat)
                 cat.Parent = parentCat
             End If
         Next
-        ' Third pass: Add top-level items to the final lists
+        For Each product In productIds.Values
+            If product.RawProduct.Parent IsNot Nothing Then
+                Dim parentProductId = QualifyName(product.RawProduct.Parent.ID, product.RawProduct.DefinedIn) ' Child products can't be defined in other files
+                Dim parentProduct = findProductById(parentProductId)
+                parentProduct.Children.Add(product)
+                product.Parent = parentProduct
+            End If
+        Next
+        For Each sup In supIds.Values
+            For Each supEntry In sup.Elements
+                Dim productId = ResolveRef(supEntry.RawSupportEntry.ProductID, sup.RawSupport.DefinedIn)
+                supEntry.Product = findProductById(productId)
+            Next
+        Next
+        For Each pol In polIds.Values
+            Dim catId = ResolveRef(pol.RawPolicy.CategoryID, pol.RawPolicy.DefinedIn)
+            Dim ownerCat = findCatById(catId)
+            If ownerCat IsNot Nothing Then
+                ownerCat.Policies.Add(pol)
+                pol.Category = ownerCat
+            End If
+            Dim supportId = ResolveRef(pol.RawPolicy.SupportedCode, pol.RawPolicy.DefinedIn)
+            pol.SupportedOn = findSupById(supportId)
+        Next
+        ' Third pass: Add items to the final lists
         For Each cat In catIds
+            FlatCategories.Add(cat.Key, cat.Value)
             If cat.Value.Parent Is Nothing Then Categories.Add(cat.Key, cat.Value)
+        Next
+        For Each product In productIds
+            FlatProducts.Add(product.Key, product.Value)
+            If product.Value.Parent Is Nothing Then Products.Add(product.Key, product.Value)
+        Next
+        For Each pol In polIds
+            Policies.Add(pol.Key, pol.Value)
+        Next
+        For Each sup In supIds
+            SupportDefinitions.Add(sup.Key, sup.Value)
         Next
         ' Purge the temporary partially-constructed items
         RawCategories.Clear()
+        RawProducts.Clear()
+        RawSupport.Clear()
+        RawPolicies.Clear()
     End Sub
+    Private Function FindInTempOrFlat(Of T)(UniqueID As String, TempDict As Dictionary(Of String, T), FlatDict As Dictionary(Of String, T)) As T
+        If TempDict.ContainsKey(UniqueID) Then
+            Return TempDict(UniqueID)
+        ElseIf FlatDict IsNot Nothing AndAlso FlatDict.ContainsKey(UniqueID) Then
+            Return FlatDict(UniqueID)
+        Else
+            Return Nothing
+        End If
+    End Function
     Private Function ResolveString(DisplayCode As String, Admx As AdmxFile) As String
         If Not DisplayCode.StartsWith("$(string.") Then Return DisplayCode
         Dim stringId = DisplayCode.Substring(9, DisplayCode.Length - 10)
@@ -75,8 +156,12 @@ Public Class AdmxBundle
     Private Function ResolveRef(Ref As String, Admx As AdmxFile) As String
         If Ref.Contains(":") Then
             Dim parts = Split(Ref, ":", 2)
-            Dim srcNamespace = Admx.Prefixes(parts(0))
-            Return srcNamespace & ":" & parts(1)
+            If Admx.Prefixes.ContainsKey(parts(0)) Then
+                Dim srcNamespace = Admx.Prefixes(parts(0))
+                Return srcNamespace & ":" & parts(1)
+            Else
+                Return Ref ' Assume a literal
+            End If
         Else
             Return QualifyName(Ref, Admx)
         End If
--- PolicyPlus/CompiledStructures.vb
+++ PolicyPlus/CompiledStructures.vb
@@ -3,5 +3,32 @@
     Public Parent As PolicyPlusCategory
     Public Children As New List(Of PolicyPlusCategory)
     Public DisplayName As String
+    Public Policies As New List(Of PolicyPlusPolicy)
     Public RawCategory As AdmxCategory
+End Class
+Public Class PolicyPlusProduct
+    Public UniqueID As String
+    Public Parent As PolicyPlusProduct
+    Public Children As New List(Of PolicyPlusProduct)
+    Public DisplayName As String
+    Public RawProduct As AdmxProduct
+End Class
+Public Class PolicyPlusSupport
+    Public UniqueID As String
+    Public DisplayName As String
+    Public Elements As New List(Of PolicyPlusSupportEntry)
+    Public RawSupport As AdmxSupportDefinition
+End Class
+Public Class PolicyPlusSupportEntry
+    Public Product As PolicyPlusProduct
+    Public RawSupportEntry As AdmxSupportEntry
+End Class
+Public Class PolicyPlusPolicy
+    Public UniqueID As String
+    Public Category As PolicyPlusCategory
+    Public DisplayName As String
+    Public DisplayExplanation As String
+    Public SupportedOn As PolicyPlusSupport
+    ' TODO: Presentation
+    Public RawPolicy As AdmxPolicy
 End Class
