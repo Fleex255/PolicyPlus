2b1ed160a855d1e49294339095898f70485ec4f2
Ben N <fleexy@mchsi.com>
2016-07-11 16:58:17 -0500

    Modify and save CMTX files

--- PolicyPlus/CmtxFile.vb
+++ PolicyPlus/CmtxFile.vb
@@ -8,10 +8,7 @@ Public Class CmtxFile
         Dim cmtx As New CmtxFile
         cmtx.SourceFile = File
         Dim xmlDoc As New XmlDocument
-        Using fFile As New IO.FileStream(File, IO.FileMode.Open, IO.FileAccess.Read)
-            fFile.Position = 3 ' Skip unknown header-like thing
-            xmlDoc.Load(fFile)
-        End Using
+        xmlDoc.Load(File)
         Dim policyComments = xmlDoc.GetElementsByTagName("policyComments")(0)
         For Each child As XmlNode In policyComments.ChildNodes
             Select Case child.LocalName
@@ -46,6 +43,24 @@ Public Class CmtxFile
         Next
         Return cmtx
     End Function
+    Public Shared Function FromCommentTable(Table As Dictionary(Of String, String)) As CmtxFile
+        Dim cmtx As New CmtxFile
+        Dim resNum As Integer = 0 ' A counter to make sure names are unique
+        Dim revPrefixes As New Dictionary(Of String, String) ' Opposite-direction prefix lookup
+        For Each kv In Table
+            Dim idParts = Split(kv.Key, ":", 2)
+            If Not revPrefixes.ContainsKey(idParts(0)) Then
+                Dim prefixId = idParts(0).Replace("."c, "_"c) & "__" & resNum
+                revPrefixes.Add(idParts(0), prefixId)
+                cmtx.Prefixes.Add(prefixId, idParts(0))
+            End If
+            Dim resourceId = idParts(0).Replace("."c, "_"c) & "__" & idParts(1) & "__" & resNum
+            cmtx.Strings.Add(resourceId, kv.Value)
+            cmtx.Comments.Add(revPrefixes(idParts(0)) & ":" & idParts(1), "$(resource." & resourceId & ")")
+            resNum += 1
+        Next
+        Return cmtx
+    End Function
     Public Function ToCommentTable() As Dictionary(Of String, String)
         Dim commentTable As New Dictionary(Of String, String)
         For Each comment In Comments
@@ -57,4 +72,49 @@ Public Class CmtxFile
         Next
         Return commentTable
     End Function
+    Public Sub Save()
+        Save(SourceFile)
+    End Sub
+    Public Sub Save(File As String)
+        Dim xml As New XmlDocument
+        Dim declaration = xml.CreateXmlDeclaration("1.0", "utf-8", "")
+        xml.AppendChild(declaration)
+        Dim policyComments = xml.CreateElement("policyComments")
+        policyComments.SetAttribute("xmlns:xsd", "http://www.w3.org/2001/XMLSchema")
+        policyComments.SetAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
+        policyComments.SetAttribute("revision", "1.0")
+        policyComments.SetAttribute("schemaVersion", "1.0")
+        policyComments.SetAttribute("xmlns", "http://www.microsoft.com/GroupPolicy/CommentDefinitions")
+        Dim policyNamespaces = xml.CreateElement("policyNamespaces")
+        For Each prefix In Prefixes
+            Dim usingElem As XmlElement = xml.CreateElement("using")
+            usingElem.SetAttribute("prefix", prefix.Key)
+            usingElem.SetAttribute("namespace", prefix.Value)
+            policyNamespaces.AppendChild(usingElem)
+        Next
+        policyComments.AppendChild(policyNamespaces)
+        Dim commentsElem = xml.CreateElement("comments")
+        Dim admTemplate = xml.CreateElement("admTemplate")
+        For Each comment In Comments
+            Dim commentElem = xml.CreateElement("comment")
+            commentElem.SetAttribute("policyRef", comment.Key)
+            commentElem.SetAttribute("commentText", comment.Value)
+            admTemplate.AppendChild(commentElem)
+        Next
+        commentsElem.AppendChild(admTemplate)
+        policyComments.AppendChild(commentsElem)
+        Dim resources = xml.CreateElement("resources")
+        resources.SetAttribute("minRequiredRevision", "1.0")
+        Dim stringTable = xml.CreateElement("stringTable")
+        For Each textval In Strings
+            Dim stringElem = xml.CreateElement("string")
+            stringElem.SetAttribute("id", textval.Key)
+            stringElem.InnerText = textval.Value
+            stringTable.AppendChild(stringElem)
+        Next
+        resources.AppendChild(stringTable)
+        policyComments.AppendChild(resources)
+        xml.AppendChild(policyComments)
+        xml.Save(File)
+    End Sub
 End Class
--- PolicyPlus/Main.vb
+++ PolicyPlus/Main.vb
@@ -1,7 +1,10 @@
 ﻿Public Class Main
     Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' Temporary testing - set a breakpoint at the "End Sub" to examine the objects with Visual Studio
-        Dim pol = PolFile.Load("C:\Windows\System32\GroupPolicy\Machine\Registry.pol")
-        Dim reg = RegistryPolicyProxy.EncapsulateKey(Microsoft.Win32.RegistryHive.LocalMachine)
+        Dim cmtx = CmtxFile.Load("\Windows\System32\GroupPolicy\Machine\comment.cmtx")
+        Dim commentDict = cmtx.ToCommentTable
+        commentDict.Add("Microsoft.Policies.BITS:BITS_Job_Timeout", "Timeout for a BITS job")
+        cmtx = CmtxFile.FromCommentTable(commentDict)
+        cmtx.Save("test.cmtx")
     End Sub
 End Class
