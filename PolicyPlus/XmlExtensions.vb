50384e369d90fb6b5d71f9ad861a468ae847ad89
glachancecmaisonneuve <glachance@cmaisonneuve.qc.ca>
2018-07-06 10:13:37 -0400

    Some admx files have incompatible default values (most often an empty string "" for numerical fields).  PolicyPlus fails when trying to parse those files.

--- PolicyPlus/XmlExtensions.vb
+++ PolicyPlus/XmlExtensions.vb
@@ -1,4 +1,5 @@
-﻿Imports System.Runtime.CompilerServices
+Imports System.ComponentModel
+Imports System.Runtime.CompilerServices
 Imports System.Xml
 Public Module XmlExtensions
     ' Convenience methods for parsing XML in AdmxFile and AdmlFile
@@ -6,6 +7,11 @@ Public Module XmlExtensions
         If Node.Attributes(Attribute) Is Nothing Then Return Nothing Else Return Node.Attributes(Attribute).Value
     End Function
     <Extension> Public Function AttributeOrDefault(Node As XmlNode, Attribute As String, DefaultVal As Object) As Object
-        If Node.Attributes(Attribute) Is Nothing Then Return DefaultVal Else Return Node.Attributes(Attribute).Value
+        If Node.Attributes(Attribute) Is Nothing Then Return DefaultVal
+        Dim converter As TypeConverter = TypeDescriptor.GetConverter(DefaultVal.GetType())
+        If converter.IsValid(Node.Attributes(Attribute).Value) Then
+            Return converter.ConvertFromString(Node.Attributes(Attribute).Value)
+        End If
+        Return DefaultVal
     End Function
 End Module
