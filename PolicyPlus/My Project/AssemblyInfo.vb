e09d1c01f14a1f6d1b75aeff2c7010166b39e77d
Ben Nordick <Fleex255@users.noreply.github.com>
2019-02-02 22:43:42 -0600

    Update AssemblyInfo.vb

--- PolicyPlus/My Project/AssemblyInfo.vb
+++ PolicyPlus/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")>
 <Assembly: AssemblyCompany("")>
 <Assembly: AssemblyProduct("Policy Plus")>
-<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyCopyright("Copyright © 2016-2019")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
